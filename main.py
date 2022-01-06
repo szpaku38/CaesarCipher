baae82f8d4cbbd8b7727cf34f1543a8ff4708bf5
szpaku38 <szpaku38@gmail.com>
2022-01-06 19:27:33 +0100

    Second stage

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+            'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(text, shift):
-    wynik = []
-    for letter in text:
-        letter_index = alphabet.index(letter)
-        shifted_index = letter_index + shift
-        wynik += alphabet[shifted_index]
-    print("Encrypted message is: " + "".join(wynik))
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        cipher_text += alphabet[new_position]
+    print(f"The encoded text is {cipher_text}")
 
-    # TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+
+def decrypt(plain_text, shift_amount):
+    decipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        decipher_text += alphabet[new_position]
+    print(f"The encoded text is {decipher_text}")
+
+    # TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
     # e.g.
-    #plain_text = "hello"
-    #shift = 5
     #cipher_text = "mjqqt"
-    # print output: "The encoded text is mjqqt"
-
-    # HINT: How do you get the index of an item in a list:
-    # https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    # 🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-    # TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+    #shift = 5
+    #plain_text = "hello"
+    # print output: "The decoded text is hello"
 
 
-encrypt(text, shift)
+    # TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(plain_text=text, shift_amount=shift)
+else:
+    print("Wrong command.")
