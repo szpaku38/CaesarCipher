c3aa6e27879386f0abb901254b2f467a325898e3
szpaku38 <szpaku38@gmail.com>
2022-01-06 20:07:17 +0100

    Third stage

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-            'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+            'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-# TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar().
+print(logo)
 
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
-    if cipher_direction == "encode":
-        for letter in start_text:
-            position = alphabet.index(letter)
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
             new_position = position + shift_amount
             end_text += alphabet[new_position]
-    elif cipher_direction == "decode":
-        for letter in start_text:
-            position = alphabet.index(letter)
-            new_position = position - shift_amount
-            end_text += alphabet[new_position]
-    else:
-        print("Wrong command.")
-    print(f"The message is {end_text}")
+        else:
+            end_text += char
+    print(f"Here's the {cipher_direction}d result: {end_text}")
 
 
-# TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
-caesar(text, shift, direction)
+still_on = True
+while still_on:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    on = input("Still on? y/n")
+    if on == "n":
+        still_on = False
